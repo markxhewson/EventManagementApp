1af329e5458fe45cfa844ffcc666bd19e1610d67
Lotho <58112436+MLotho@users.noreply.github.com>
2024-03-21 03:40:03 +0000

    fix for default nav issues

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
                             <Route path="/signup" element={<Signup />} />
                             <Route path="/login" element={<Login />} />
                             <Route path="/verification" element={<Verification />} />
-                            <Route path="*" element={<PageNotFound />} />
+                            <Route path="*" element={<DefaultSignup />} />
                         </>
                     )
                 }
